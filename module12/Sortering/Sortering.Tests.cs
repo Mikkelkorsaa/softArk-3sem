3d9e1c513e409ca61f379a583ddbf88f29eebcb0
asgermunk <75023079+asgermunk@users.noreply.github.com>
2025-03-31 12:54:39 +0200

    finished module 12

--- module12/Sortering/Sortering.Tests.cs
+++ module12/Sortering/Sortering.Tests.cs
@@ -29,20 +29,22 @@ namespace Sortering.Tests
             CollectionAssert.AreEqual(new int[] { 11, 15, 18, 34, 45, 67 }, array);
         }
 
-        // [TestMethod]
-        // public void TestQuickSort()
-        // {
-        //     int[] array = new int[] { 34, 18, 15, 45, 67, 11 };
-        //     QuickSort.Sort(array);
-        //     CollectionAssert.AreEqual(new int[] { 11, 15, 18, 34, 45, 67}, array);
-        // }
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            int[] array = new int[] { 34, 18, 15, 45, 67, 11 };
+            QuickSort.Sort(array);
+            System.Console.WriteLine("QuickSort");
+            System.Console.WriteLine(string.Join(", ", array));
+            CollectionAssert.AreEqual(new int[] { 11, 15, 18, 34, 45, 67}, array);
+        }
 
-        // [TestMethod]
-        // public void TestMergeSort()
-        // {
-        //     int[] array = new int[] { 34, 18, 15, 45, 67, 11 };
-        //     MergeSort.Sort(array);
-        //     CollectionAssert.AreEqual(new int[] { 11, 15, 18, 34, 45, 67}, array);
-        // }
+       [TestMethod]
+        public void TestMergeSort()
+        {
+            List<int> list = new List<int> { 34, 18, 15, 45, 67, 11 };
+            MergeSort.Sort(list);
+            CollectionAssert.AreEqual(new List<int> { 11, 15, 18, 34, 45, 67 }, list);
+        }
     }
 }
