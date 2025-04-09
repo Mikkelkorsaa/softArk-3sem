df23d2c55fc528df1d56002c9ecac35275b5651c
asgermunk <75023079+asgermunk@users.noreply.github.com>
2025-04-09 08:30:50 +0200

    new gitignore

--- module13/eksempel/obj/Debug/net6.0/eksempel.AssemblyInfo.cs
+++ module13/eksempel/obj/Debug/net6.0/eksempel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eksempel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006100a41fc7e034ffd32c066e5f8ed4b0a09187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277fc17bb1efdb001d06a0619d54905b40399fc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("eksempel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eksempel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
